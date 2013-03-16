02b5b7a956f90f454583dedbe4065c3170141649
David Sosby <david@sosby.org>
2013-03-16 10:14:05 -0500

    Remove references to original project

--- run.scm
+++ run.scm
@@ -1,8 +1,9 @@
-(use awful spiffy)
-(include "ezersatz.scm")
-(import ezersatz)
+(use awful spiffy ezersatz)
 
 (enable-sxml #f)
+(define DefaultPageTitle (make-parameter "TODO:PageTitle"))
+(define DefaultPageTemplate (make-parameter "index.tmpl"))
+
 (when (development-mode?)
   (debug-file "debug.log"))
 
@@ -14,19 +15,14 @@
        (development-mode?)))
 
 ;; Fill the template with the given data
-(define (standard-page data #!key (file "index.tmpl") (page-title "BigButton"))
+(define (standard-page data #!key (file (DefaultPageTemplate)) (page-title (DefaultPageTitle)))
     (ezersatz file
 	      (let* ([data (as-model data)]
 		     [data (alist-cons 'page_title page-title data)]
 		     [data (if (show-dev-mode?) (alist-cons 'devmode #t data) data)])
 		data)))
 
-(define (get-greeting-string salutation #!optional [name "World"])
-  (string-append salutation ", " name))
-
 (define-page (main-page-path)
   (lambda () 
-    (let* ([whom ($ 'whom "World")]
-	   [greeting (get-greeting-string "Hello" whom)])
-      (debug whom)
-      (standard-page greeting page-title: "Big Button Homepage"))))
+    (debug "This is how you log stuff")
+    (standard-page "I don't do much" page-title: "Hello World!")))
